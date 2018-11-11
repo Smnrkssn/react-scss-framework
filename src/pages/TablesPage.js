c2911227b720d72dda00c1e65055f9353964c19d
Simon Eriksson <dv16sen@cs.umu.se>
2018-11-11 15:24:06 +0100

    Added support for color tables.

--- src/pages/TablesPage.js
+++ src/pages/TablesPage.js
@@ -1,56 +1,57 @@
 import React, {Component} from "react";
 import {Page} from "../site-components/Page";
 import SortableTable from "../components/SortableTable";
+import {colors} from "../services/colors";
 
 class TablesPage extends Component {
     render() {
         return (
             <Page>
                 <h3>Tables</h3>
-                <table className="table striped clickable mb-30">
+                <table className={`table striped sortable clickable mb-30`}>
                     <thead>
-                        <tr>
-                            <th>Heading 1</th>
-                            <th>Heading 2</th>
-                            <th>Heading 3</th>
-                        </tr>
+                    <tr>
+                        <th>Heading 1</th>
+                        <th>Heading 2</th>
+                        <th>Heading 3</th>
+                    </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>Data 1</td>
-                            <td>Data 2</td>
-                            <td>Data 3</td>
-                        </tr>
-                        <tr>
-                            <td>Data 1</td>
-                            <td>Data 2</td>
-                            <td>Data 3</td>
-                        </tr>
-                        <tr>
-                            <td>Data 1</td>
-                            <td>Data 2</td>
-                            <td>Data 3</td>
-                        </tr>
-                        <tr>
-                            <td>Data 1</td>
-                            <td>Data 2</td>
-                            <td>Data 3</td>
-                        </tr>
+                    <tr>
+                        <td>Data 1</td>
+                        <td>Data 2</td>
+                        <td>Data 3</td>
+                    </tr>
+                    <tr>
+                        <td>Data 1</td>
+                        <td>Data 2</td>
+                        <td>Data 3</td>
+                    </tr>
+                    <tr>
+                        <td>Data 1</td>
+                        <td>Data 2</td>
+                        <td>Data 3</td>
+                    </tr>
+                    <tr>
+                        <td>Data 1</td>
+                        <td>Data 2</td>
+                        <td>Data 3</td>
+                    </tr>
                     </tbody>
                 </table>
                 <h3>Sortable Table</h3>
                 <SortableTable className="mb-30">
                     <thead>
                         <tr>
-                            <th className="no-sort">Heading 1</th>
-                            <th>Heading 2</th>
+                            <th className="no-sort">Column with sort disabled</th>
+                            <th className="bg-color-b-red">Heading 2</th>
                             <th>Heading 3</th>
                         </tr>
                     </thead>
                     <tbody className="head">
                         <tr className="head">
                             <td className="head">Data 2</td>
-                            <td>12</td>
+                            <td>23</td>
                             <td>Female</td>
                         </tr>
                         <tr>
@@ -61,7 +62,7 @@ class TablesPage extends Component {
                         <tr>
                             <td>Data 1</td>
                             <td>37</td>
-                            <td>Female</td>
+                            <td>Robot</td>
                         </tr>
                     </tbody>
                 </SortableTable>
@@ -94,7 +95,7 @@ class TablesPage extends Component {
                     </tbody>
                 </table>
                 <h3>List Table</h3>
-                <table className="list-table">
+                <table className="list-table mb-30">
                     <tbody>
                     <tr>
                         <td>List item 1</td>
@@ -106,6 +107,40 @@ class TablesPage extends Component {
                     </tr>
                     </tbody>
                 </table>
+                <h3>Color Tables (works for all table types)</h3>
+                {[...colors, ...colors.map(color => `${color} dark`)].map((color, i) => (
+                    <table key={i} className={`table striped sortable clickable ${color} mb-30`}>
+                        <thead>
+                        <tr>
+                            <th>Heading 1</th>
+                            <th>Heading 2</th>
+                            <th>Heading 3</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>Data 1</td>
+                            <td>Data 2</td>
+                            <td>Data 3</td>
+                        </tr>
+                        <tr>
+                            <td>Data 1</td>
+                            <td>Data 2</td>
+                            <td>Data 3</td>
+                        </tr>
+                        <tr>
+                            <td>Data 1</td>
+                            <td>Data 2</td>
+                            <td>Data 3</td>
+                        </tr>
+                        <tr>
+                            <td>Data 1</td>
+                            <td>Data 2</td>
+                            <td>Data 3</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                ))}
             </Page>
         );
     }
